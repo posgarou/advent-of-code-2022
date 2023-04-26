a835e8eea737e1c18f4aa9f9f7bff4dfae81f537
Ryan Mitchell <ryan@playsidelinesports.com>
2023-04-25 22:14:46 -0400

    refactor(day01): switch elf reader to use for loop

--- days/01/src/read_elves_from_file.rs
+++ days/01/src/read_elves_from_file.rs
@@ -4,38 +4,40 @@ use std::path::Path;
 use thiserror::Error;
 
 use crate::elf::Elf;
-use crate::food::Food;
 
 #[derive(Debug, Error)]
 pub enum ElfParserError {
-  #[error("Error reading file: {0}")]
-  FileReadError(#[from] std::io::Error),
-  #[error("Error parsing integer: {0}")]
-  ParseError(#[from] std::num::ParseIntError),
+    #[error("Error reading file: {0}")]
+    FileReadError(#[from] std::io::Error),
+    #[error("Error parsing integer: {0}")]
+    ParseError(#[from] std::num::ParseIntError),
 }
 
-pub fn read_elves_from_file(filename: &str) -> Result<Vec<Elf>, std::io::Error> {
+pub fn read_elves_from_file(filename: &str) -> Result<Vec<Elf>, ElfParserError> {
     let mut elves = vec![];
-    let mut lines = read_lines(filename)?;
+    let lines = read_lines(filename)?;
 
-    loop {
-        let foods = lines
-            .by_ref()
-            .take_while(|line| !line.as_ref().unwrap().is_empty())
-            .map(|line| Food::new(line.unwrap().parse().unwrap()))
-            .collect::<Vec<Food>>();
+    let mut current_food_items: Vec<i32> = vec![];
 
-        if foods.is_empty() {
-            break;
+    for line in lines.into_iter() {
+        let line = line?;
+
+        if line.is_empty() && !current_food_items.is_empty() {
+            elves.push(Elf::from(current_food_items));
+            current_food_items = vec![];
+            continue;
         }
 
-        let elf = Elf::from(foods);
-        elves.push(elf);
+        let calories = line.parse()?;
+        current_food_items.push(calories);
+    }
+
+    if !current_food_items.is_empty() {
+        elves.push(Elf::from(current_food_items));
     }
 
     Ok(elves)
 }
-
 
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
@@ -43,4 +45,39 @@ where
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_read_elves_from_file() {
+        let fixture = "fixtures/elves.txt";
+        let elves = read_elves_from_file(fixture).unwrap();
+        assert_eq!(
+            elves,
+            vec![
+                Elf::from(vec![1000, 2000, 3000]),
+                Elf::from(vec![4000]),
+                Elf::from(vec![5000, 6000]),
+                Elf::from(vec![7000, 8000, 9000]),
+                Elf::from(vec![10000]),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_read_lines() {
+        let fixture = "fixtures/elves.txt";
+        let lines = read_lines(fixture).unwrap();
+        let lines: Vec<String> = lines.map(|line| line.unwrap()).collect();
+        assert_eq!(
+            lines,
+            vec![
+                "1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000",
+                "", "10000"
+            ]
+        );
+    }
 }
